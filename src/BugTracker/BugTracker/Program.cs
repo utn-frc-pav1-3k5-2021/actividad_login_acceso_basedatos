5dc642fc01e6e52ed2cd712a4847e89128836e11
Santidmlv <santidmlv@gmail.com>
2021-08-18 22:40:52 -0300

    Tarea finalizada

--- src/BugTracker/BugTracker/Program.cs
+++ src/BugTracker/BugTracker/Program.cs
@@ -17,7 +17,7 @@ namespace BugTracker
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            Application.Run(new frmPrincipal());
         }
     }
 }

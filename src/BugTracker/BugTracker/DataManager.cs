b6a7a6f63f68a09b5c81ec0a8ae4845b54cf4d50
Victor Bazan <bazan.victor@hotmail.com>
2021-08-25 22:17:53 -0300

    se agrega codigo

--- src/BugTracker/BugTracker/DataManager.cs
+++ src/BugTracker/BugTracker/DataManager.cs
@@ -10,7 +10,7 @@ public class DataManager
     private static DataManager instance;
     public DataManager()
     {
-        string_conexion = "Data Source=.\\SQLEXPRESS;Initial Catalog=BugTracker;Integrated Security=true;";
+        string_conexion = @"Data Source=M26\SQLEXPRESS;Initial Catalog=BT;Integrated Security=True";
     }
 
     // Con el siguiente método aplicamos el patrón singleton
